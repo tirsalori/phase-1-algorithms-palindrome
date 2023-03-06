a7a4d738057e24e391e361fa4ac7dae64503cc21
Tirsa St. Fort <lifelovelori@gmail.com>
2023-03-05 16:31:46 -0800

    Completed lab

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let newString = ""
+  for (let i = word.length-1; i >= 0; i--) {
+    newString += word[i]
+  }
+  if (newString === word) {
+    return true
+  } else {
+    return false
+  }
 }
 
-/* 
-  Add your pseudocode here
-*/
+isPalindrome("robot")
 
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
